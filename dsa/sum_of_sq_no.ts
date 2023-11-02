d3c031a3f987eb9cdcb01cdf0a4951c668ce9df3
prasad-mude-7 <prasadmude7@gmail.com>
2023-11-02 13:23:28 +0530

    dsa_started

---
+++ dsa/sum_of_sq_no.ts
@@ -0,0 +1,14 @@
+unction judgeSquareSum(c: number): boolean {
+    for (let a = 0; a * a <= c; a++) {
+        const b = Math.sqrt(c - a * a);
+        if (b === Math.floor(b)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+const c = 3;
+const result = judgeSquareSum(c);
+console.log(result);
